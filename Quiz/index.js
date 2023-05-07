aa6f61ea05470cbd229c10176c007089e7cc1ca2
Anjolaoluwa Adeyemi <91416834+anjolagithub@users.noreply.github.com>
2023-05-07 16:38:50 +0100

    Add files via upload

---
+++ Quiz/index.js
@@ -0,0 +1,18 @@
+// const toggler = document.querySelector('.navbar-toggler');
+// const navbarNav = document.querySelector('.navbar-nav');
+
+// toggler.addEventListener('click', function () {
+//     navbarNav.classList.toggle('active');
+// });
+
+// // toggler.addEventListener('click', function (event) {
+// //     event.preventDefault();
+// //     navbarNav.classList.toggle('active');
+
+// //     // Add logic to fold and unfold the navbar
+// //     if (navbarNav.classList.contains('active')) {
+// //         toggler.innerHTML = '<span class="navbar-toggler-icon"></span> Close';
+// //     } else {
+// //         toggler.innerHTML = '<span class="navbar-toggler-icon"></span> Menu';
+// //     }
+// });
